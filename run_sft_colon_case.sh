478cfc162b8eb6abdefe796492e976229a8c33a7
huangfeihong <huangfh@xm-innovision.com>
2026-07-10 18:15:25 +0800

    更新胃镜&肠镜微调脚本

---
+++ run_sft_colon_case.sh
@@ -0,0 +1,39 @@
+python supervised_finetuning.py \
+    --model_name_or_path /home/inno/.cache/modelscope/hub/models/Qwen/Qwen3.5-4B/ \
+    --train_file_dir /media/inno/LLM/肠镜/report/V1/train/ \
+    --validation_file_dir /media/inno/LLM/肠镜/report/V1/val/ \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --do_train \
+    --do_eval \
+    --template_name qwen \
+    --use_peft True \
+    --max_train_samples 1000 \
+    --max_eval_samples -1 \
+    --model_max_length 4096 \
+    --num_train_epochs 10 \
+    --learning_rate 1e-4 \
+    --warmup_steps 5 \
+    --weight_decay 0.05 \
+    --logging_strategy steps \
+    --logging_steps 10 \
+    --eval_steps 50 \
+    --eval_strategy epoch \
+    --save_steps 500 \
+    --save_strategy epoch \
+    --save_total_limit 13 \
+    --gradient_accumulation_steps 8 \
+    --preprocessing_num_workers 4 \
+    --output_dir /media/inno/work_dirs/LLM/MedicalGPT/colon/outputs-sft-qwen3-4b-instruct-v1 \
+    --ddp_timeout 30000 \
+    --logging_first_step True \
+    --target_modules all \
+    --lora_rank 16 \
+    --lora_alpha 32 \
+    --lora_dropout 0.05 \
+    --torch_dtype bfloat16 \
+    --bf16 \
+    --report_to tensorboard \
+    --ddp_find_unused_parameters False \
+    --gradient_checkpointing True \
+    --cache_dir ./cache --flash_attn True
